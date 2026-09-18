fe50314e7d5f5128db1861f6d238aa1363b5d412
FCS-dev <107736390+FCS-dev@users.noreply.github.com>
2026-09-18 03:46:30 +0200

    build: Update API URL

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-  API_BASE: "http://localhost:8080/api/v1",
+  // API_BASE: "http://localhost:8080/api/v1",
+  API_BASE: "https://api-misfichas.onrender.com/api/v1",
   CURRENCY_SYMBOL: "€",
 };
